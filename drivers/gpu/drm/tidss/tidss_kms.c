1a197097d5bd97a63761fa872c9e41f775748324
Chun Jiao Zhao <chunjiao.zhao@siemens.com>
2025-11-24 16:03:54 +0800

    Revert "drm/tidss: Update encoder/bridge chain connect model"
    This reverts commit c932ced6b58524eeb948ff252f6512cd6bcb9d9f.
    
    The reverted commit has compatibility issues with the DP bridge chip
    (TC358767) driver, causing blurry display and screen artifacts on DP.
    
    This is a workaround that reverts that commit to avoid the blurry display
    and artifacts.
    
    Signed-off-by: Chun Jiao Zhao <chunjiao.zhao@siemens.com>

--- drivers/gpu/drm/tidss/tidss_kms.c
+++ drivers/gpu/drm/tidss/tidss_kms.c
@@ -188,6 +188,7 @@ static int tidss_dispc_modeset_init(struct tidss_device *tidss)
 	for (i = 0; i < num_pipes; ++i) {
 		struct tidss_plane *tplane;
 		struct tidss_crtc *tcrtc;
+		struct drm_encoder *enc;
 		u32 hw_plane_id = feat->vid_order[tidss->num_planes];
 		int ret;
 
@@ -210,13 +211,16 @@ static int tidss_dispc_modeset_init(struct tidss_device *tidss)
 
 		tidss->crtcs[tidss->num_crtcs++] = &tcrtc->crtc;
 
-		ret = tidss_encoder_create(tidss, pipes[i].bridge,
-					   pipes[i].enc_type,
+		enc = tidss_encoder_create(tidss, pipes[i].enc_type,
 					   1 << tcrtc->crtc.index);
-		if (ret) {
+		if (IS_ERR(enc)) {
 			dev_err(tidss->dev, "encoder create failed\n");
-			return ret;
+			return PTR_ERR(enc);
 		}
+
+		ret = drm_bridge_attach(enc, pipes[i].bridge, NULL, 0);
+		if (ret)
+			return ret;
 	}
 
 	/* create overlay planes of the leftover planes */
